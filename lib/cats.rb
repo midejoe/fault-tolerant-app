c0319efe2221ab6ce4b3c27ea904a69678841d41
Joseph Ariyo <midejoseph24@gmail.com>
2025-04-10 19:25:22 -0500

    Add Sinatra files

---
+++ lib/cats.rb
@@ -0,0 +1,18 @@
+# frozen_string_literal: true
+
+require 'sinatra/base'
+require 'sinatra/json'
+require 'net/http'
+
+module Cats
+  class Web < Sinatra::Base
+    configure do
+      set :url, URI('http://thecatapi.com/api/images/get').freeze
+      sleep(1 + rand(5))
+    end
+
+    get '/' do
+      json url: Net::HTTP.get_response(settings.url)['location']
+    end
+  end
+end
